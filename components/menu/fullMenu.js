26fbc68870aba2b762db0f760146aaae92ee8635
Diego Vazquez <diegovazquezny@gmail.com>
2021-01-30 00:26:07 -0500

    add api route for menul

--- components/menu/fullMenu.js
+++ components/menu/fullMenu.js
@@ -1,37 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MenuItem from './menuItem';
 import styles from '../../styles/FullMenu.module.css';
 import { Paper } from '@material-ui/core';
-import menuList from './menuList';
 import MenuNav from '../../components/menu/menuNav';
 
 export default function FullMenu() {
-  return (
-    <Paper className={styles.root}>
-      <MenuNav/>
-      {
-        menuList.map((type, i) => {
-          return (
-            <div className={styles.categoryContainer} key={`t${i}`} id={type.id}>
-              <div className={styles.menuItemsContainer}>
-                <h3 className={styles.categoryName}>{type.category}</h3>
-                <div className={styles.menuItemsWrapper}>
-                  {type.options.map((option, i) => {
-                    return (
-                      <MenuItem
-                        name={option.name}
-                        price={option.price}
-                        description={option.description}
-                        key={`o${i}`}
-                      />
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          )
+  const [fetchedMenu, setFetchedMenu] = useState(false);
+  const [menuList, setMenuList] = useState([]);
+  
+  useEffect(() => {
+    if (!fetchedMenu) {
+      fetch('/api/menu')
+        .then(res => res.json())
+        .then(data => {
+          setMenuList(data);
+          setFetchedMenu(true);
         })
+        .catch(err => console.log(err));
+    }
+  })
+
+  return (
+    <>
+      { fetchedMenu 
+        ?
+        <Paper className={styles.root}>
+          <MenuNav/>
+          {
+            menuList.map((type, i) => {
+              return (
+                <div className={styles.categoryContainer} key={`t${i}`} id={type.id}>
+                  <div className={styles.menuItemsContainer}>
+                    <h3 className={styles.categoryName}>{type.category}</h3>
+                    <div className={styles.menuItemsWrapper}>
+                      {type.options.map((option, i) => {
+                        return (
+                          <MenuItem
+                            name={option.name}
+                            price={option.price}
+                            description={option.description}
+                            key={`o${i}`}
+                          />
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </Paper>
+        : 'Loading'
       }
-    </Paper>
+    </>
   )
 }
---
+++ pages/api/menuList.js
@@ -0,0 +1,128 @@
+const menuList = [
+  {
+    category: 'Pizza',
+    id: 'pizza',
+    options: [
+      {
+        name: 'Cheese Pizza',
+        price: 15.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: 'Pepperoni Pizza',
+        price: 17.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: 'Meat Pizza',
+        price: 18.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: 'Supreme Pizza',
+        price: 18.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: `GrandMa's Pie`,
+        price: 18.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: `Chef's Delight Pizza`,
+        price: 19.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: `Italian Margherita`,
+        price: 17.99,
+        description: 'This is a generic description for pizza'
+      },
+      {
+        name: `Buffalo Chicken Pizza`,
+        price: 18.99,
+        description: 'This is a generic description for pizza'
+      },
+    ]
+  },
+  {
+    category: 'Heros',
+    id: 'heros',
+    options: [
+      {
+        name: 'Meatball Hero',
+        price: 7.99,
+        description: 'Fresh meatballs, mozzarella, and grilled onions'
+      },
+      {
+        name: 'Sausage and Peppers Hero',
+        price: 9.99,
+        description: 'Italian sausage, green peppers, and onions'
+      },
+      {
+        name: 'Grilled Chicken Hero',
+        price: 8.99,
+        description: 'Grilled chicken, red peppers, and onions'
+      },
+      {
+        name: 'Philly Cheesesteak',
+        price: 9.99,
+        description: 'Sliced sirloin, provolone, green peppers, and onions'
+      },
+    ]
+  },
+  {
+    category: 'Desserts',
+    id: 'desserts',
+    options: [
+      {
+        name: 'Chocolate Chip Cookies',
+        price: 4.99,
+        description: 'Three large freshly baked chocolate chip cookies'
+      },
+      {
+        name: 'Brownie with Walnuts',
+        price: 4.99,
+        description: 'Freshly baked brownie with walnuts'
+      },
+      {
+        name: 'Cannoli',
+        price: 4.99,
+        description: 'Classic Italian cannoli'
+      },
+      {
+        name: 'Crepe with Banana and Nutella',
+        price: 4.99,
+        description: 'Homemade crepe with banana and Nutella'
+      }
+    ]
+  },
+  {
+    category: 'Drinks',
+    id: 'drinks',
+    options: [
+      {
+        name: 'Soft Drink',
+        price: 2.99,
+        description: 'Two liter bottle of soft drink'
+      },
+      {
+        name: 'Iced Coffee',
+        price: 3.99,
+        description: '16oz of freshly brewed iced coffee'
+      },
+      {
+        name: 'Bottled Water',
+        price: 1.99,
+        description: '20oz bottle of water'
+      },
+      {
+        name: 'Orange Juice',
+        price: 3.99,
+        description: '16oz of freshly squeezed orange juice'
+      }
+    ]
+  }
+];
+
+export default menuList;
