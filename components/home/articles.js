2ccc8cd85d7e1ef2a33ad3e4095e66522681fab4
Diego Vazquez <diegovazquezny@gmail.com>
2021-02-02 22:36:50 -0500

    get articles from db

--- components/home/articles.js
+++ components/home/articles.js
@@ -3,38 +3,7 @@ import styles from '../../styles/Articles.module.css';
 import { Paper } from '@material-ui/core';
 import Link from 'next/link';
 
-const articles = [
-  {
-    title: 'World Class Desserts',
-    description: `
-      Nothing is better than finishing off a few slices of pizza with a
-      delicious dessert. Try our ice cream or our cookies.
-    `,
-    img: './articles/brownies.png',
-    url: '/menu#desserts',
-    id: 'dess'
-  },
-  {
-    title: 'Delicious Heros',
-    description: `
-      Not in the mood for pizza? We got you covered. Our diverse selection
-      of heros will definitely hit the spot.
-    `,
-    img: './articles/sandwich.png',
-    url: '/menu#heros',
-    id: 'sand'
-  },
-  {
-    title: 'Ice Cold Drinks',
-    description: `Try our freshly brewed iced coffee, our amazing freshly squeezed orange juice,
-      or a thirst quenching soft drink.`,
-    img: './articles/icedcoffee.png',
-    url: '/menu#drinks',
-    id: 'drink'
-  }
-];
-
-export default function Articles() {
+export default function Articles({ articleData }) {
   return (
     <div className={styles.root}>
       {
