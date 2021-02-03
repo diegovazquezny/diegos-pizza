a9dec8e673d4e55c6bd344d8a931105a41da0c00
Diego Vazquez <diegovazquezny@gmail.com>
2021-02-02 22:40:26 -0500

    fix typos and links

--- components/menu/menuNav.js
+++ components/menu/menuNav.js
@@ -3,10 +3,10 @@ import styles from '../../styles/MenuNav.module.css';
 export default function MenuNav() {
   return (
     <div className={styles.root}>
-      <a href='#pizza'><button className={styles.button}>Pizza</button></a>
-      <a href='#heros'><button className={styles.button}>Heros</button></a>
-      <a href='#desserts'><button className={styles.button}>Desserts</button></a>
-      <a href='#drinks'><button className={styles.button}>Drinks</button></a>
+      <a href='#Pizza'><button className={styles.button}>Pizza</button></a>
+      <a href='#Heros'><button className={styles.button}>Heros</button></a>
+      <a href='#Desserts'><button className={styles.button}>Desserts</button></a>
+      <a href='#Drinks'><button className={styles.button}>Drinks</button></a>
     </div>
   )
 }
--- pages/api/data.js
+++ pages/api/data.js
@@ -3,42 +3,42 @@ export const carouselItems = [
     title: 'Supreme Pizza',
     description: 'Fresh vegetables and pepperoni',
     img: '/carousel/supr.png',
-    url: '/menu#pizza',
+    url: '/menu#Pizza',
     id: 'vege'
   },
   {
     title: 'Meatzza Meatzza!',
     description: 'Packed with delicious protein',
     img: '/carousel/meat.png',
-    url: '/menu#pizza',
+    url: '/menu#Pizza',
     id: 'meat'
   },
   {
     title: 'Classic Pepperoni',
     description: 'So simple, so tasty',
     img: '/carousel/pepp.png',
-    url: '/menu#pizza',
+    url: '/menu#Pizza',
     id: 'pepp'
   },
   {
     title: `GrandMa's Pie`,
     description: 'A little slice of Italian home cooking',
     img: '/carousel/grma.png',
-    url: '/menu#pizza',
+    url: '/menu#Pizza',
     id: 'grma'
   },
   {
     title: 'Italian Margherita',
     description: 'Mouth watering elegance',
     img: '/carousel/marg.png',
-    url: '/menu#pizza',
+    url: '/menu#Pizza',
     id :'marg'
   },
   {
     title: `Chef's Delight`,
     description: 'Peppers, onions, jalepeños, & chicken',
     img: '/carousel/chef.png',
-    url: '/menu#pizza',
+    url: '/menu#Pizza',
     id: 'chef'
   }
 ];
@@ -48,20 +48,20 @@ export const articles = [
     title: 'World Class Desserts',
     description: `
       Nothing is better than finishing off a few slices of pizza with a
-      delicous dessert. Try our brownies or cannolis.
+      delicious dessert. Try our brownies or cannolis.
     `,
     img: './articles/brownies.png',
-    url: '/menu#desserts',
+    url: '/menu#Desserts',
     id: 'dess'
   },
   {
-    title: 'Delicous Heros',
+    title: 'Delicious Heros',
     description: `
       Not in the mood for pizza? We got you covered. Our diverse selection
       of heros will definitely hit the spot.
     `,
     img: './articles/sandwich.png',
-    url: '/menu#heros',
+    url: '/menu#Heros',
     id: 'sand'
   },
   {
@@ -69,7 +69,7 @@ export const articles = [
     description: `Try our freshly brewed iced coffee, our amazing freshly squeezed orange juice,
       or a thirst quenching soft drink.`,
     img: './articles/icedcoffee.png',
-    url: '/menu#drinks',
+    url: '/menu#Drinks',
     id: 'drink'
   }
 ];
